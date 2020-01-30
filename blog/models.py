f3906dfcaa52130cb05281eaaefff17330e8f6be
Rob Greene <greener2@gmail.com>
2020-01-30 22:19:46 +0000

    Adding in the structure around inheriting templates, and styling too. Added Post model.

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,12 @@
+import datetime
+
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=140, null=False, blank=False)
+    subtitle = models.CharField(max_length=140, null=False)
+    body = models.TextField(blank=False, null=False)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    date_published = models.DateTimeField(default=datetime.datetime.utcnow)
