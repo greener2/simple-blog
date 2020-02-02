bd56dbf8502496002edc1e34071f69656ec9c428
Rob Greene <greener2@gmail.com>
2020-02-02 22:10:20 +0000

    ADDED SEARCH! Plus some better styling, or at least that's a matter of opinion.

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from blog import views
 
 urlpatterns = [
-    path('', views.PostList.as_view(), name="home"),
-    path('<int:pk>/', views.PostDetail.as_view(), name="post_detail")
+    path('', views.index, name="home"),
+    path('<int:pk>/', views.PostDetail.as_view(), name="post_detail"),
+    path('tags/<str:name>/', views.tag_search, name="tag_search")
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,17 +1,27 @@
+from django.db.models import Q
+from django.shortcuts import render
 from django.views import generic
 
 from blog.models import Post, Tag
 
 
-class PostList(generic.ListView):
-    queryset = Post.objects.all().order_by('-date_published')
-    template_name = "blog/index.html"
+def index(request):
+    tags = Tag.objects.all()
+    if request.method == "GET":
+        search_query = request.GET.get("search_box", None)
+        print(search_query)
+        if not search_query:
+            template = "blog/index.html"
+            posts = Post.objects.all().order_by('-date_published')
+        else:
+            template = "blog/search_result.html"
+            posts = Post.objects.filter(Q(title__icontains=search_query) | Q(subtitle__contains=search_query) | Q(body__contains=search_query)).order_by('-date_published')
+    else:
+        search_query = ""
+        template = "blog/index.html"
+        posts = Post.objects.all().order_by('-date_published')
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["tags"] = Tag.objects.all()
-        return context
-
+    return render(request, template, {"posts": posts, "tags": tags, "search_query": search_query})
 
 
 class PostDetail(generic.DetailView):
@@ -22,3 +32,10 @@ class PostDetail(generic.DetailView):
         context = super().get_context_data(**kwargs)
         context["tags"] = Tag.objects.all()
         return context
+
+
+def tag_search(request, name):
+    posts = Post.objects.filter(tags__name=name)
+    all_tags = Tag.objects.all()
+
+    return render(request, "blog/tag-search.html", context={"posts": posts, "tag_name": name, "tags": all_tags})
