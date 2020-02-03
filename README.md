90b9948051185e03888cae525318c701a8360eb0
Rob Greene <robert.greene@giant.ie>
2020-02-03 14:08:32 +0000

    Adding the last few finishing touches before it goes to Heroku (e.g. reading from environment vars)

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # simple-blog
 A simple blog built in Django to chronicle my adventures in learning fullstack development.
+
+## Movitation
+I wanted to learn how to make a simple blogging platform in Django, as a lot of Python backend is written in Django. 
+I also wanted to challenge myself to learn how to style a not-incredibly-ugly website (I'm very willing to admit my 
+frontend skills need a *LOT* of work!) and to create something that I would actually use.
+
+## Structure
+It's a regular Django app called `blog`, with the project name being `blogsite`. It is accessible from the URL 
+`http://127.0.0.1:8000/blog` with the admin page being at `http://127.0.0.1:8000/admin`.
+
+## Features
+The blog has the following very simple features.
+
+- Tags for tagging posts, and searching for posts that have particular tags.
+- Search function to search for a term in the title, subtitle or body of each post.
+- Correct HTML rendering for the post body (HTML is stored and is rendered correctly in the browser.)
+
+Planned for the future:
+
+- Automated testing
+- A nicer admin/CMS setup for inputting blog posts
+- A comment section (although that might involve an over-estimation of how many people are going to read it!) 
+
+## License
+MIT Licensed, so you should be able to do whatever you want with it, if you choose.
+
+## Contact
+Author is Rob Greene (greener2-at-gmail.com) if you have any questions or suggestions. 
--- blogsite/settings.py
+++ blogsite/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+from decouple import config
+from dj_database_url import parse as dburl
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,12 +22,14 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'xc9b3q!y=ygu(lp@doy@h+b2*dw9u61@*!&xkzl_o!lv(m^we-'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config("DEBUG")
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "greener-blog.herokuapp.com",
+]
 
 
 # Application definition
@@ -37,7 +41,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'tinymce',
     "blog",
 ]
 
@@ -75,11 +78,10 @@ WSGI_APPLICATION = 'blogsite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+default_dburl = "sqlite:///" + os.path.join(BASE_DIR, "db.sqlite3")
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': config("DATABASE_URL", default=default_dburl, cast=dburl)
 }
 
 
@@ -120,3 +122,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
