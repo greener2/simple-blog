5cee3fb78ceb6673239d973d86d0271330a2b66c
Rob Greene <greener2@gmail.com>
2020-02-04 22:01:57 +0000

    Added markdown editor for admin page, and the template tag to render it in templates

--- blog/admin.py
+++ blog/admin.py
@@ -1,11 +1,18 @@
 from django.contrib import admin
-from blog import models
+from pagedown.widgets import AdminPagedownWidget
 
+from blog import models
+import django.db.models
+    
 
 class PostAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'date_published')
     search_fields = ['title', 'subtitle', 'body', 'author']
     filter_horizontal = ("tags", )
+
+    formfield_overrides = {
+        django.db.models.TextField: {"widget": AdminPagedownWidget}
+    }
 
 
 admin.site.register(models.Post, PostAdmin)
